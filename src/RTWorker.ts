02be54694a27b73a436287adcfb0d6d670f71222
Danielle Smith <code@danini.dev>
2024-02-06 13:27:32 +0200

    add sphere

--- src/RTWorker.ts
+++ src/RTWorker.ts
@@ -1,6 +1,7 @@
 import Camera from "./Camera"
 import Color from "./Color"
 import Ray from "./Ray"
+import Vec3 from "./Vec3"
 import writeColor from "./writeColor"
 
 let width: number
@@ -8,7 +9,18 @@ let height: number
 let camera: Camera
 let buffer: SharedArrayBuffer
 
+function hitSphere(center: Vec3, radius: number, r: Ray): boolean {
+  const oc = r.origin.minus(center)
+  const a = r.direction.lengthSquared
+  const b = 2.0 * oc.dot(r.direction)
+  const c = oc.lengthSquared - radius*radius
+  const discriminant = b*b - 4*a*c
+  return discriminant >= 0
+}
+
 function rayColor(r: Ray) {
+  if (hitSphere(new Vec3(0, 0, -1), 0.5, r)) return new Color(1, 0, 0)
+
   const unitDirection = r.direction.unit
   const a = 0.5*(unitDirection.y + 1.0)
   return new Color(1.0, 1.0, 1.0).scale(1.0-a).add(new Color(0.5, 0.7, 1.0).scale(a))
