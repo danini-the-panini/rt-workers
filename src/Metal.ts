929d9fbbe0e7388500b39e9b5948b840a5d51b42
Danielle Smith <code@danini.dev>
2024-02-06 16:40:31 +0200

    add fuzzy metal

--- src/Metal.ts
+++ src/Metal.ts
@@ -2,14 +2,15 @@ import Color from "./Color";
 import { HitRecord } from "./IHittable";
 import IMaterial, { Scatter } from "./IMaterial";
 import Ray from "./Ray";
+import Vec3 from "./Vec3";
 
 export default class Metal implements IMaterial {
-  constructor(public albedo: Color) {}
+  constructor(public albedo: Color, public fuzz: number) {}
 
   scatter(rIn: Ray, rec: HitRecord): Scatter | null {
     const reflected = rIn.direction.unit.reflect(rec.normal)
     return {
-      scattered: new Ray(rec.p, reflected),
+      scattered: new Ray(rec.p, reflected.add(Vec3.randomUnitVector().scale(this.fuzz))),
       attenuation: this.albedo
     }
   }
@@ -17,11 +18,12 @@ export default class Metal implements IMaterial {
   get serialize() {
     return {
       type: 'metal' as const,
-      albedo: this.albedo.serialize
+      albedo: this.albedo.serialize,
+      fuzz: this.fuzz
     }
   }
 
-  static deserialize({ albedo }: typeof Metal.prototype.serialize) {
-    return new Metal(Color.deserialize(albedo))
+  static deserialize({ albedo, fuzz }: typeof Metal.prototype.serialize) {
+    return new Metal(Color.deserialize(albedo), fuzz)
   }
 }
